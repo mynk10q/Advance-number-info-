bbdc0a130aee4d2473817c8112975924d3ee7965
mynk10q <amit1100941@gmail.com>
2026-03-21 20:31:16 +0530

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,50 @@
+export default async function handler(req, res) {
+
+  const { key, term } = req.query;
+
+  if (key !== "mynkx") {
+    return res.json({
+      status: false,
+      message: "Invalid key"
+    });
+  }
+
+  if (!term) {
+    return res.json({
+      status: false,
+      message: "No term"
+    });
+  }
+
+  try {
+
+    const response = await fetch("https://leakosintapi.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        token: "8167229089:lJEQg6UI",
+        request: term,
+        limit: 100,
+        lang: "en"
+      })
+    });
+
+    const data = await response.json();
+
+    res.json({
+      status: true,
+      result: data
+    });
+
+  } catch (e) {
+
+    res.json({
+      status: false,
+      message: "API Down"
+    });
+
+  }
+
+}
