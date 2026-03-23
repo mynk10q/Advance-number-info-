99a4c4a3eba5daac40e62d29d11a586e874e909c
mynk10q <amit1100941@gmail.com>
2026-03-23 16:10:06 +0530

    Simplify response format in search API

--- api/search.js
+++ api/search.js
@@ -33,10 +33,7 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    res.json({
-      status: true,
-      result: data
-    });
+    res.json(data);
 
   } catch (e) {
 
