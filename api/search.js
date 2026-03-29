2b425ee96fac9290b96af19aeb2df44263590bb9
mynk10q <amit1100941@gmail.com>
2026-03-29 14:01:21 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        token: "8670404667:yg7tAYFV",
+        token: "6706754806:mztInj7j",
         request: term,
         limit: 100,
         lang: "en"
